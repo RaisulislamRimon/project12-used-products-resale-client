3e6ed804a38f618658786acbacc2439b7faf0bdd
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-26 11:59:04 +0600

    navbar & notFound page setup

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { MdReportGmailerrorred } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-center">
-        This content is not available right now!
-      </h1>
+    <div className="min-h-screen">
+      <div>
+        <MdReportGmailerrorred className="text-5xl text-center text-red-600 w-full my-10" />
+        <h1 className="text-xl md:text-3xl lg:text-5xl font-bold text-center">
+          This content is not available right now{" "}
+          <span className="text-red-600">!</span>
+        </h1>
+        <Link to="/">
+          <p className="md:text-2xl text-center my-10 hover:text-blue-600 border border-warning p-5 md:w-1/3 mx-auto rounded-xl">
+            Go to Homepage
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main/Main";
+import Home from "../../Pages/Home/Home";
 import NotFound from "../../Pages/Shared/NotFound/NotFound";
 
 const router = createBrowserRouter([
@@ -8,6 +9,16 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
