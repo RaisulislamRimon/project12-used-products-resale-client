d17c3f0db34e2ab6966b5492246dfdacdd630fe7
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-27 20:35:31 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,24 +6,23 @@ const Banner = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
           <img
-            src="https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            // src="https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src="https://images.pexels.com/photos/5883535/pexels-photo-5883535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             className="max-w-lg h-full rounded-lg shadow-2xl"
             alt=""
           />
           <div>
             <h1 className="text-5xl mb-7 font-bold">
-              <div>Second Hand Products,</div>
+              <div>Second Hand Books,</div>
               <div className="mt-4">Buy & Sell Now</div>
             </h1>
             <p className="py-6">
-              Best places to buy and sell almost everything. This is a best
-              place for buying and selling online assets. We are great with
-              communication and have the best interest for both sellers and
-              buyers.
+              Best places to buy and sell almost every kinds of books. This is
+              the best place for buying and selling second hands books. We are
+              great with communication and have the best interest for both
+              sellers and buyers.
             </p>
-            <button className="btn btn-accent text-white">
-              See All Categories
-            </button>
+            <button className="btn btn-accent">See All Books Categories</button>
           </div>
         </div>
       </div>
