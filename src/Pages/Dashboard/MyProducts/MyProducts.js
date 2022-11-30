24c04dd07f47e030195ab53a07ef9338c121c25c
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-30 09:18:28 +0600

    delete my product

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 import Loading from "../../Shared/Loading/Loading";
+import Swal from "sweetalert2";
 
 const MyProducts = () => {
   const { user } = useContext(AuthContext);
@@ -30,6 +31,25 @@ const MyProducts = () => {
     return <div>Something went wrong...</div>;
   }
 
+  const handleDelete = (bookDelete) => {
+    fetch(`http://localhost:5000/my-books/${bookDelete._id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.deletedCount > 0) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your book has been deleted.",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+        }
+      });
+  };
+
   return (
     <div>
       <h1 className="text-center text-3xl my-3">My Products</h1>
@@ -55,7 +75,13 @@ const MyProducts = () => {
                   {/* <td>Advertise | Delete</td> */}
                   <td>
                     {" "}
-                    {myBook.available === "unsold" && "Advertise | "} Delete
+                    {myBook.available === "unsold" && "Advertise | "}
+                    <span
+                      onClick={() => handleDelete(myBook)}
+                      className="text-red-500 cursor-pointer"
+                    >
+                      Delete
+                    </span>
                   </td>
                 </tr>
               ))}
