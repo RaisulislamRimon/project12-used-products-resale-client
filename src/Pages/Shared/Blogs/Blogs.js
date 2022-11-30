42be1b86cabec14f3cb766a7dfa81b7f0d275d28
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-30 23:58:48 +0600

    blogs updated

--- src/Pages/Shared/Blogs/Blogs.js
+++ src/Pages/Shared/Blogs/Blogs.js
@@ -3,18 +3,42 @@ import React from "react";
 const Blogs = () => {
   return (
     <div>
-      <h1 className="text-2xl">
+      <h1 className="text-red-400 p-3 text-2xl">
         What are the different ways to manage a state in a React application?
       </h1>
-      <h3 className="text-xl"></h3>
-      <h1 className="text-2xl">How does prototypical inheritance work?</h1>
-      <h3 className="text-xl"></h3>
-      <h1 className="text-2xl">
+      <h3 className="text-xl p-2">
+        There are many different ways to manage a state in a react application.
+        There are many libraries to handle states in react applicaions. The Four
+        Kinds of React State to Manage Local state. Global state. Server state.
+        URL state.
+      </h3>
+      <h1 className="text-red-400 p-3 text-2xl">
+        How does prototypical inheritance work?
+      </h1>
+      <h3 className="text-xl p-2">
+        The Prototypal Inheritance is a feature in javascript used to add
+        methods and properties in objects. It is a method by which an object can
+        inherit the properties and methods of another object. Traditionally, in
+        order to get and set the [[Prototype]] of an object, we use Object.
+        getPrototypeOf and Object.
+      </h3>
+      <h1 className="text-red-400 p-3 text-2xl">
         What is a unit test? Why should we write unit tests?
       </h1>
-      <h3 className="text-xl"></h3>
-      <h1 className="text-2xl">React vs. Angular vs. Vue?</h1>
-      <h3 className="text-xl"></h3>
+      <h3 className="text-xl p-2">
+        The main objective of unit testing is to isolate written code to test
+        and determine if it works as intended. Unit testing is an important step
+        in the development process, because if done correctly, it can help
+        detect early flaws in code which may be more difficult to find in later
+        testing stages.
+      </h3>
+      <h1 className="text-red-400 p-3 text-2xl">React vs. Angular vs. Vue?</h1>
+      <h3 className="text-xl p-2">
+        Vue provides higher customizability and hence is easier to learn than
+        Angular or React. Further, Vue has an overlap with Angular and React
+        with respect to their functionality like the use of components. Hence,
+        the transition to Vue from either of the two is an easy option.
+      </h3>
     </div>
   );
 };
