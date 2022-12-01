b75c8511c82ac5efe71e2668ce973d5efba17109
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-02 01:05:36 +0600

    update new component heading

--- src/Pages/Home/New/New.js
+++ src/Pages/Home/New/New.js
@@ -6,14 +6,14 @@ const New = () => {
       <div className="mt-20">
         <div className="card lg:card-side ">
           <div className="card-body lg:w-1/3">
-            <h2 className="card-title text-5xl mb-7">
-              Online Buy Sell Club <br /> For Your Learning
+            <h2 className="card-title md:text-2xl lg:text-5xl mb-7">
+              Online Buy Sell Club <br /> For Your Reading & Learning
             </h2>
             <p className="">
-              This is the best online learning platform. Here you can buy and
-              sell all kinds of books. If you want to sell your sencond hand
-              book, you can always sell it here. We have a lot of customers all
-              over the world.
+              This is the best online reading & learning platform. Here you can
+              buy and sell all kinds of books. If you want to sell your sencond
+              hand book, you can always sell it here. We have a lot of customers
+              all over the world.
             </p>
             <div className="">
               <button className="btn btn-primary text-white text-sm">
