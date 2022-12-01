b6a89b6370b2ae97e07ef17b54152fd190ee7507
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-02 01:25:55 +0600

    added not found image in not found page/component

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -8,9 +8,14 @@ const NotFound = () => {
       <div>
         <MdReportGmailerrorred className="text-5xl text-center text-red-600 w-full my-10" />
         <h1 className="text-xl md:text-3xl lg:text-5xl font-bold text-center">
-          This content is not available right now{" "}
+          This page is not available right now{" "}
           <span className="text-red-600">!</span>
         </h1>
+        <img
+          src="https://i.ibb.co/8K6ZXxm/not-found.png"
+          className="w-2/4 mx-auto my-10"
+          alt="notFoundImage"
+        />
         <Link to="/">
           <p className="md:text-2xl text-center my-10 hover:text-blue-600 border border-warning p-5 md:w-1/3 mx-auto rounded-xl">
             Go to Homepage
