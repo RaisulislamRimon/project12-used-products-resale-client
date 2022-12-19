a63d574c04c935b2c991613bb0a3519b21487152
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-19 14:50:19 +0600

    update reported items

--- src/Pages/Dashboard/ReportedItems/ReportedItems.js
+++ src/Pages/Dashboard/ReportedItems/ReportedItems.js
@@ -4,6 +4,9 @@ const ReportedItems = () => {
   return (
     <div>
       <h1 className="text-center text-3xl my-5 underline">Reported Items</h1>
+      <div className="flex justify-center items-center h-96">
+        <h1 className="text-lg font-semibold text-gray-700">Coming Soon...</h1>
+      </div>
     </div>
   );
 };
