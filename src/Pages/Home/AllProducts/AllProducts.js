f99fe6e3e502aad32d5b03fb53529ddede63c7da
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-19 14:31:03 +0600

    added loading error & showing all products(books)

--- src/Pages/Home/AllProducts/AllProducts.js
+++ src/Pages/Home/AllProducts/AllProducts.js
@@ -1,9 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Loading from "../../Shared/Loading/Loading";
+import LoadingError from "../../Shared/Loading/LoadingError";
+import ShowBooksByCategoriesCard from "../ShowBooksByCategoriesCard/ShowBooksByCategoriesCard";
 
 const AllProducts = () => {
+  const {
+    isLoading,
+    error,
+    data: allBooks = [],
+  } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("http://localhost:5000/all-books").then((res) => res.json()),
+  });
+
+  if (isLoading) return <Loading />;
+
+  if (error) return <LoadingError />;
   return (
     <div>
-      <h1 className="text-center text-3xl my-5">All Products</h1>
+      <h1 className="text-center text-3xl my-5 underline">All Products</h1>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto">
+        {allBooks.map((book) => (
+          <ShowBooksByCategoriesCard book={book} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Shared/Loading/LoadingError.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingError = () => {
+  return (
+    <div>
+      <h3 className="flex justify-center items-center text-red-600 p-20 font-semibold text-2xl bg-white">
+        An error has occurred: Please check your internet connection...
+      </h3>
+    </div>
+  );
+};
+
+export default LoadingError;
